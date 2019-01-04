4c7c2139f3af336d3b534faf065afdb25911c693
Momentanius <guilhermegustavogohr@gmail.com>
2019-01-04 14:19:42 -0200

    Trabalhando com Json

--- Looney_Lips/LooneyLips.gd
+++ Looney_Lips/LooneyLips.gd
@@ -2,32 +2,15 @@ extends Node2D
 
 var player_words = [] # words that the player will type
 #Isso se chama dictionary. Eles são criados com { e são semelhantes a JSONs. 
-var template = [{
-		"prompt":["a name", "a city", "a person", "a feeling"],
-		"story": "%s is the most important figure in %s. %s must really %s him, otherwise you would not be playing this game."
-		},
-		{
-		"prompt":["the first part of the meme", "the second part of the meme", "the third part of the meme"],
-		"story": "What is a %s? A miserable little pile of %s. But enough talk, %s."
-		},
-		{
-		"prompt":["a fruit", "a taste", "another fruit"],
-		"story": "Do you like %s? I think they are pretty %s, but not as good as %s."
-		},
-		{
-		"prompt":["a game", "another gamet"],
-		"story": "I really like %s, but %s is a better game."
-		}
-		
-		]
 var current_story
+var strings # all the text that's going to be shown to the player.
 
 func _ready():
-	randomize()
-	current_story = template[randi() % template.size()]
-	
+	set_random_story()
+	strings = get_from_json("other_strings.json")
 	$Blackboard/TextBox.grab_focus()
-	$Blackboard/StoryText.text = "Greetings.\nThis is a new game by Momentanius.\nPlease, answer the questions in order. \nCan I have " + current_story.prompt[player_words.size()] + " please? "
+	$Blackboard/StoryText.text = strings["intro"] + " "
+	prompt_player()
 	
 	# O $ funciona como um atalho para get_node("Blackboard/StoryText")
 #	$Blackboard/StoryText.bbcode_text = story % prompt;
@@ -40,13 +23,31 @@ func _on_TextureButton_pressed():
 		var new_text = $Blackboard/TextBox.get_text()
 		_on_TextBox_text_entered(new_text)
 
+
 func _on_TextBox_text_entered(new_text):
 	player_words.append(new_text)
 	$Blackboard/TextBox.text = ""
+	$Blackboard/StoryText.text = ""
 	check_player_word_lenght()
 
 func prompt_player():
-	$Blackboard/StoryText.text = "Can I have " + current_story.prompt[player_words.size()] + " please? "
+	var next_prompt = current_story["prompt"][player_words.size()]
+	$Blackboard/StoryText.text += (strings["prompt"] % next_prompt)
+	
+
+func set_random_story():
+	var stories = get_from_json("stories.json")
+	randomize()
+	current_story = stories[randi() % stories.size()]
+	pass
+
+func get_from_json(filename):
+	var file = File.new() # the file object
+	file.open(filename, File.READ) #assumes the file exists
+	var text = file.get_as_text()
+	var data = parse_json(text)
+	file.close() #lembre-se de fechar os arquivos
+	return data
 	
 
 func is_story_done():
@@ -63,6 +64,7 @@ func check_player_word_lenght():
 # story % player_words faz ele selecionar o próximo valor em um array automaticamente.
 func tell_story():
 	$Blackboard/StoryText.text = current_story.story % player_words
+	$Blackboard/TextureButton/ButtonLabel.text = strings["again"]
 	end_game()
 
 #Queue Free remove um nó da cena. Excelente para remover elementos quanto eles estiverem prontos.
