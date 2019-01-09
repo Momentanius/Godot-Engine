20d72793e825e5e2886c73562af0b77c54cbf9ff
Momentanius <guilhermegustavogohr@gmail.com>
2019-01-09 10:20:17 -0200

    Moedas e vidas funcionais, novas animacoes adicionadas

--- Hoppy Days/Scripts/GUI.gd
+++ Hoppy Days/Scripts/GUI.gd
@@ -3,6 +3,10 @@ extends CanvasLayer
 func _ready():
 	Global.GUI = self
 	
-func update_GUI(lives):
+func update_GUI(coins, lives):
+	$Banner/HBoxContainer/CoinCount.text = str(coins)
 	$Banner/HBoxContainer/LifeCount.text = str(lives)
 	pass
+
+func playAnimation(animation):
+	$AnimationPlayer.play(animation)
--- Hoppy Days/Scripts/GameState.gd
+++ Hoppy Days/Scripts/GameState.gd
@@ -2,7 +2,9 @@ extends Node2D
 
 #Quando iniciar o jogo através do GameState.gd, seta o Gamestate de Global como este cara.
 export var starting_lives = 3
+export var coin_target = 4 #how many coins for an extra live
 var lives
+var coins = 0
 
 func _ready():
 	Global.GameState = self
@@ -11,13 +13,27 @@ func _ready():
 	
 
 func update_GUI():
-	Global.GUI.update_GUI(lives)
+	Global.GUI.update_GUI(coins, lives)
 
 func hurt():
 	lives -= 1
 	if lives < 0:
 		end_game()
 	Global.Player.hurt()
+	Global.GUI.playAnimation("Hurt")
+	update_GUI()
+	
+func coin_up():
+	coins+=1
+	Global.GUI.playAnimation("CoinPulse")
+	update_GUI()
+	var multiple_of_coin_target = (coins % coin_target) == 0
+	if multiple_of_coin_target:
+		Global.GUI.playAnimation("LifePulse")
+		life_up()
+
+func life_up():
+	lives+=1
 	update_GUI()
 
 func end_game():
