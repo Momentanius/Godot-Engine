40ad4b821bd35e25da5f29ecaa55f4248a8e2f57
Momentanius <guilhermegustavogohr@gmail.com>
2019-01-17 16:52:17 -0200

    Novas mudanças em NumberPad

--- Heist Meisters/Scripts/Global.gd
+++ Heist Meisters/Scripts/Global.gd
@@ -8,3 +8,6 @@ var destinations
 # Asset Links. Must be changed manually
 var nightvision_on_sfx = "res://SFX/nightvision.wav"
 var nightvision_of_sfx = "res://SFX/nightvision_off.wav"
+
+var red_light = "res://GFX/Interface/PNG/dotRed.png"
+var green_light = "res://GFX/Interface/PNG/dotGreen.png"
